cc0d8bc196a177cab66e634b09cad513c0a6b1c0
lucky <takuto.yoshikai@gmail.com>
2021-01-03 17:49:53 +0900

    argparse

--- git_extract/git_extract.py
+++ git_extract/git_extract.py
@@ -1,12 +1,9 @@
 from git import Repo
 from io import BytesIO
 import os
+import argparse
 
-repo = Repo("/home/lucky/obin")
-commits = list(repo.iter_commits("master"))
-exported_dir = "./tmp"
 
-i = 1
 
 def file_write(filename, content):
     file_dir = os.path.dirname(filename)
@@ -14,17 +11,37 @@ def file_write(filename, content):
         os.makedirs(file_dir)
     with open(filename, "wb") as f:
         f.write(content)
-for commit in commits:
-    print("=" * 30)
-    for file_name in commit.stats.files:
-        print(file_name)
-        contents = None
-        try:
-            contents = repo.git.show('{}:{}'.format(commit.hexsha, file_name))
-        except:
-            continue
-        d = exported_dir + "/" + str(i)
-        os.makedirs(d)
-        file_write(d + "/" + file_name, contents.encode("utf-8", "surrogateescape"))
-        i = i + 1
 
+def extract_repo(repo_path, branch, exported_dir="./tmp"):
+    repo = Repo(repo_path)
+    commits = list(repo.iter_commits(branch))
+    i = 1
+    for commit in commits:
+        print("=" * 30)
+        for file_name in commit.stats.files:
+            print(file_name)
+            contents = None
+            try:
+                contents = repo.git.show('{}:{}'.format(commit.hexsha, file_name))
+            except:
+                continue
+            d = exported_dir + "/" + str(i)
+            os.makedirs(d)
+            file_write(d + "/" + file_name, contents.encode("utf-8", "surrogateescape"))
+            i = i + 1
+
+
+def main():
+    parser = argparse.ArgumentParser(description="This is an extractor of git repository.")
+    parser.add_argument("repo")
+    parser.add_argument("branch")
+    parser.add_argument("-o", "--output")
+    args = parser.parse_args()
+    if args.output == None:
+        extract_repo(args.repo, args.branch)
+    else:
+        extract_repo(args.repo, args.branch, args.output)
+
+
+if __name__ == "__main__":
+    main()
