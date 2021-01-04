4212736c70d2cc0a8b812285216c7e9218b23653
lucky <takuto.yoshikai@gmail.com>
2021-01-04 11:49:13 +0900

    json save

--- esight/esight.py
+++ esight/esight.py
@@ -1,8 +1,7 @@
 from git import Repo
 import os
 import argparse
-
-
+import json
 
 def file_write(filename, content):
     file_dir = os.path.dirname(filename)
@@ -15,6 +14,7 @@ def extract_repo(repo_path, branch, exported_dir="./tmp"):
     repo = Repo(repo_path)
     commits = list(repo.iter_commits(branch))
     i = 1
+    json_data = {}
     for commit in commits:
         print("=" * 30)
         for file_name in commit.stats.files:
@@ -28,7 +28,14 @@ def extract_repo(repo_path, branch, exported_dir="./tmp"):
                 continue
             d = exported_dir + "/" + str(len(commits) - i)
             file_write(d + "/" + file_name, contents.encode("utf-8", "surrogateescape"))
+        json_data[str(len(commits) - i)] = {
+            "name": commit.author.name,
+            "email": commit.author.email
+        }
         i = i + 1
+    with open(exported_dir + "/git.json", mode="w") as f:
+        json_data = json.dumps(json_data)
+        f.write(json_data)
 
 
 def main():
